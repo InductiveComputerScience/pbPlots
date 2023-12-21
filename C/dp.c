de8e87648b8c67def52ee83d87a7cc6f9a5f69af
Martin F. Johansen <martinfjohansen@inductive.no>
2023-12-21 17:15:38 +0100

    intrinsics

--- C/dp.c
+++ C/dp.c
@@ -1,5 +1,7 @@
 #include "pbPlots.h"
 
+#include <immintrin.h>
+
 void stosd(void *p, long long count, long long val);
 
 RGBABitmapImage *CreateImage(double w, double h, RGBA *color){
@@ -24,7 +26,7 @@ RGBABitmapImage *CreateImage(double w, double h, RGBA *color){
   return image;
 }
 void SetPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
-  if(x >= 0.0 && x < image->xLength && y >= 0.0 && y < ImageHeight(image)){
+  if(x >= 0.0 && x < image->xLength && y >= 0.0 && y < image->yLength){
     int pixel = x + y * image->xLength;
     int r = Round(color->r * 255);
     int g = Round(color->g * 255);
@@ -37,6 +39,9 @@ void SetPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
 double Round(double x){
   return floor(x + 0.5);
 }
+
+//typedef double v2df __attribute__ ((vector_size (2)));
+
 void DrawPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
   double ra, ga, ba, aa;
   double rb, gb, bb, ab;
@@ -44,10 +49,15 @@ void DrawPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
   RGBA newColor;
   uint32_t oldColor;
   int pixel;
+	int r, g, b, a;
+	double t1, t2, f;
+	__m128d A, B, C;
 
-  double f = 0.00392156862;
+  f = 0.00392156862;
 
-  if(x >= 0.0 && x < image->xLength && y >= 0.0 && y < ImageHeight(image)){
+	// https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html
+
+  if(x >= 0.0 && x < image->xLength && y >= 0.0 && y < image->yLength){
     ra = color->r;
     ga = color->g;
     ba = color->b;
@@ -60,11 +70,11 @@ void DrawPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
     bb = ((oldColor >> 8) & 0xFF) * f;
     ab = ((oldColor >> 0) & 0xFF) * f;
 
-    double t1 = ab*(1.0 - aa);
+    t1 = ab*(1.0 - aa);
 
     ao = aa + t1;
 
-    double t2 = 1/ao;
+    t2 = 1/ao;
 
     ro = (ra*aa + rb*t1)*t2;
     go = (ga*aa + gb*t1)*t2;
@@ -75,10 +85,26 @@ void DrawPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
     newColor.b = bo;
     newColor.a = ao;
 
-    int r = Round(newColor.r * 255);
-    int g = Round(newColor.g * 255);
-    int b = Round(newColor.b * 255);
-    int a = Round(newColor.a * 255);
+    //r = Round(newColor.r * 255);
+    //g = Round(newColor.g * 255);
+    //b = Round(newColor.b * 255);
+    //a = Round(newColor.a * 255);
+
+		A[0] = 0.0;
+		A[1] = 0.0;
+		B[0] = newColor.r * 255;
+		B[1] = 0.0;
+		C = _mm_round_sd(A, B, _MM_FROUND_TO_NEAREST_INT);
+		r = C[0];
+		B[0] = newColor.g * 255;
+		C = _mm_round_sd(A, B, _MM_FROUND_TO_NEAREST_INT);
+		g = C[0];
+		B[0] = newColor.b * 255;
+		C = _mm_round_sd(A, B, _MM_FROUND_TO_NEAREST_INT);
+		b = C[0];
+		B[0] = newColor.a * 255;
+		C = _mm_round_sd(A, B, _MM_FROUND_TO_NEAREST_INT);
+		a = C[0];
 
     image->pixels[pixel] = (r << 24) | (g << 16) | (b << 8) | a;
   }
