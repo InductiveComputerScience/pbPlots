78e83d69dc7f8d8c7901fca6326a69db5bb64e20
Martin F. Johansen <martinfjohansen@inductive.no>
2023-12-22 15:25:12 +0100

    optimized version

--- C-intel-sse/dp.c
+++ C-intel-sse/dp.c
@@ -1,7 +1,6 @@
 #include "pbPlots.h"
 
 // SSE4.2
-// https://stackoverflow.com/questions/11228855/header-files-for-x86-simd-intrinsics
 #include <nmmintrin.h>
 
 void stosd(void *p, long long count, long long val);
@@ -41,45 +40,6 @@ void SetPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
 double Round(double x){
   return floor(x + 0.5);
 }
-
-/*
-#include <stdint.h>
-#include <stdalign.h>
-#include <stdio.h>
-
-void p128_u8(__m128i in) {
-    alignas(16) uint8_t v[16];
-    _mm_store_si128((__m128i*)v, in);
-    printf("v16_u8: %d %d %d %d | %d %d %d %d | %d %d %d %d | %d %d %d %d\n",
-           v[0], v[1],  v[2],  v[3],  v[4],  v[5],  v[6],  v[7],
-           v[8], v[9], v[10], v[11], v[12], v[13], v[14], v[15]);
-}
-
-void p128_u16(__m128i in) {
-    alignas(16) uint16_t v[8];
-    _mm_store_si128((__m128i*)v, in);
-    printf("v8_u16: %d %d %d %d,  %d %d %d %d\n", v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
-}
-
-void p128_u32(__m128i in) {
-    alignas(16) uint32_t v[4];
-    _mm_store_si128((__m128i*)v, in);
-    printf("v4_u32: %d %d %d %d\n", v[0], v[1], v[2], v[3]);
-}
-
-void p128_u64(__m128i in) {
-    alignas(16) unsigned long long v[2];  // uint64_t might give format-string warnings with %llx; it's just long in some ABIs
-    _mm_store_si128((__m128i*)v, in);
-    printf("v2_u64: %lld %lld\n", v[0], v[1]);
-}
-
-void p128_f32(__m128 in) {
-    alignas(16) float v[4];
-    _mm_store_ps((__m128*)v, in);
-    printf("v4_f32: %f %f %f %f\n", v[0], v[1], v[2], v[3]);
-}
-*/
-
 void DrawPixel(RGBABitmapImage *image, double x, double y, RGBA *color){
   double ra, ga, ba, aa;
   double rb, gb, bb, ab;
